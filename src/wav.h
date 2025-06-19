b1fdfcaea5e999bfa146118d67a50888e24eb673
Jason Hall <jbhall78@gmail.com>
2025-06-19 13:16:52 -0400

    Made wav file loading more robust

--- src/wav.h
+++ src/wav.h
@@ -4,15 +4,15 @@
 typedef struct {
     uint16_t channels;
     uint32_t sample_rate;
-    uint32_t data_rate;
-    uint16_t data_block_size;
+    uint32_t byte_rate; // Renamed for clarity, often called byte_rate
+    uint16_t block_align; // Renamed for clarity, often called block_align
     uint16_t bits_per_sample;
-    uint32_t channel_mask;
-    uint32_t size;
+    uint32_t channel_mask; // Only for WAVE_FORMAT_EXTENSIBLE
+    uint32_t data_size; // Renamed from size for clarity
     char *data;
 } wav_t;
 
-wav_t *wav_new(char *);
-wav_t *wav_free(wav_t *);
+wav_t *wav_new(const char *);
+void wav_free(wav_t *);
 
 #endif
